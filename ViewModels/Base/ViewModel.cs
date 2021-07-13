b75a81a00e1affb70f8b47a95662940eea11c66b
Dmitry Zatsarinny <dmitry_zatsarinny@mail.ru>
2021-07-13 20:48:23 +0300

    Интерфейс IDispose

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace MVVM_XML_RS.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
     
@@ -18,6 +18,19 @@ namespace MVVM_XML_RS.ViewModels.Base
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
         }
     }
 }
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_XML_RS.ViewModels
+{
+    class MainWindowViewModel
+    {
+    }
+}
