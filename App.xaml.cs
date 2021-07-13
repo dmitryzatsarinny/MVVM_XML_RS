38745c2426d98165940e47edce5f5f7e23918bd2
Dmitry Zatsarinny <dmitry_zatsarinny@mail.ru>
2021-07-13 19:42:42 +0300

    Добавлены папки

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace MVVM_XML_RS
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace MVVM_XML_RS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
