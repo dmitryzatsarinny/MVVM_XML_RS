a6c2c61e329646b4fb5b86701546ce31b291e579
Dmitry Zatsarinny <dmitry_zatsarinny@mail.ru>
2021-07-14 16:00:11 +0300

    Доделана первичная разметка

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,16 +8,29 @@ namespace MVVM_XML_RS.ViewModels
     internal class MainWindowViewModel: ViewModel
     {
         #region Заголовок окна
-
         private string _Title = "Reabilitation Param Changer";
-
         /// <summary>Заголовок окна</summary>
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
-
+        #endregion
+        #region Версия приложения
+        private string _Version = "Version " + "1.0";
+        public string Version
+        {
+            get => _Version;
+            set => Set(ref _Version, value);
+        }
+        #endregion
+        #region Статус
+        private string _Status = "Ready";
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
         #endregion
     }
 }
