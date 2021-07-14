276bb322805c35d4dcc055210d30d52e9d5a08fd
Dmitry Zatsarinny <dmitry_zatsarinny@mail.ru>
2021-07-14 11:43:34 +0300

    Сделана менюшка

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace MVVM_XML_RS.ViewModels.Base
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using MVVM_XML_RS.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MVVM_XML_RS.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel: ViewModel
     {
+        #region Заголовок окна
+
+        private string _Title = "Reabilitation Param Changer";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
